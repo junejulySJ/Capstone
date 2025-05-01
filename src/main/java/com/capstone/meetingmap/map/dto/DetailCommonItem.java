9a7d1558822b050bb47eafe512f9c78d14624f59
Marondochi <risumt69@gmail.com>
2025-05-01 10:52:15 +0900

    DetailCommonItem 구조 수정

--- src/main/java/com/capstone/meetingmap/map/dto/DetailCommonItem.java
+++ src/main/java/com/capstone/meetingmap/map/dto/DetailCommonItem.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class DetailCommonItem {
     private String contentid;
     private String contenttypeid;
+    private String booktour;
     private String createdtime;
     private String homepage;
     private String modifiedtime;
